0a9bc68133dd98cddccb760c2b8dbb7f75995332
Samyunwei <259375702@qq.com>
2019-12-10 01:56:58 +0800

    修改了grpc的一部分问题

---
+++ gestureRecognitionClient/script/genproto.sh
@@ -0,0 +1,4 @@
+ ../../packages/Grpc.Tools.2.25.0/tools/windows_x64/protoc.exe -I../Protos --csharp_out=../Protos --grpc_out=../Protos --plugin=../../packages/Grpc.Tools.2.25.0/tools/windows_x64/grpc_csharp_plugin.exe ../Protos/RecognitionServer.proto
+
+
+
